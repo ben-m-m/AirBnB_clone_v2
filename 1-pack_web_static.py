866f3e4d16f48693b27131159f62781a75ae1b7f
ben-m-m <mainaben23@gmail.com>
2023-07-12 05:48:04 +0000

    runs once

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,11 +5,12 @@ web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 from fabric.api import *
 from datetime import datetime
+from fabric.decorators import runs_once
 
 
 @runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
+    """generates .tgz archive from the contents of the web_static folder"""
     local("mkdir -p versions")
     path = ("versions/web_static_{}.tgz"
             .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
