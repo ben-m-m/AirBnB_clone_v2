07b2bf307172644533f7de766b0ee1f6e4cc6f31
ben-m-m <mainaben23@gmail.com>
2023-07-28 11:34:19 +0300

    number route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -38,7 +38,7 @@ def python_route(text):
     return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return "{} is a number".format(int n)
 
